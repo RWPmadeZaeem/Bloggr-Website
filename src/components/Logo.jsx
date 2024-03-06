8532b4b1e1767976c6b8f1577b11efb9cb11787a
RWPmadeZaeem <mkhalid.bscs21seecs@seecs.edu.pk>
2024-03-06 20:22:46 +0500

    second commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,13 @@
-function Logo({ width = "100px" }) {
+
+import { FaBlog } from 'react-icons/fa'; // Import the desired icon from Font Awesome
+
+function BloggrLogo() {
   return (
-      <h1 className="font-semibold text-lg">Bloggr</h1>
+    <div className="flex items-center text-white">
+      <FaBlog className="text-4xl mr-2" /> {/* Icon */}
+      <span className="text-xl font-bold">Bloggr</span> {/* Bloggr text */}
+    </div>
   );
 }
 
-export default Logo;
+export default BloggrLogo;
